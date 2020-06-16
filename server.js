398de5a8ae5dd2d31ebbe7ccea06698e2c485b67
Harit Hatawong <jharit@MacBook-Air-khxng-Harit.local>
2020-06-16 09:50:37 +0700

    change res

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cors());
 
 
 app.get('/',(req,res)=> {
-    res.send(database.users);
+    res.send('database.users');
 })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req,res, db,bcrypt)})
